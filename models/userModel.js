5d0ca4f451cb0a7fd2a1e8841b62cd7f6f5b13eb
YuJi-2023 <sophia.jiyu@gmail.com>
2024-06-06 11:29:48 +1000

    update userModel

--- models/userModel.js
+++ models/userModel.js
@@ -3,12 +3,16 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    required: [true, "please enter an user name"],
+    required: [true, "please enter a username"],
+  },
+  email: {
+    type: String,
+    required: [true, "please enter an email address"],
   },
   password: {
     type: String,
     required: [true, "please enter a password"],
-  }
+  },
 });
 const UserModel = mongoose.model("Users", userSchema);
 module.exports = UserModel;
