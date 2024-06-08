d200e3ee0b6f4e780bc99c5455a4bad6474f285a
YuJi-2023 <sophia.jiyu@gmail.com>
2024-06-08 12:07:19 +1000

    add tracker route to handle newexpense POST request

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var signupRouter = require("./routes/signup");
 var loginRouter = require("./routes/login");
 var usersRouter = require("./routes/users");
 var todosRouter = require("./routes/todos");
+var trackerRouter = require("./routes/tracker");
 require("dotenv").config();
 var app = express();
 app.use(cors());
@@ -31,6 +32,7 @@ app.use("/signup", signupRouter);
 app.use("/login", loginRouter);
 app.use("/users", usersRouter);
 app.use("/todos", todosRouter);
+app.use("/tracker", trackerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
